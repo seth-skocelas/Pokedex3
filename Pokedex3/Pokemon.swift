f588ec3995fd11fd3a7f8380c92c1e9327739032
seth-skocelas <s.skocelas@gmail.com>
2017-02-09 21:24:58 -0600

    Lessons 4 - 9: Displaying all Pokemon with Picture and Name

---
+++ Pokedex3/Pokemon.swift
@@ -0,0 +1,35 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by Seth Skocelas on 2/8/17.
+//  Copyright © 2017 Seth Skocelas. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    fileprivate var _name: String!
+    fileprivate var _pokedexId: Int!
+    
+    var name: String {
+        
+        return _name
+        
+    }
+    
+    var pokedexId: Int {
+        
+        return _pokedexId
+        
+    }
+    
+    init(name: String, pokedexId: Int) {
+        
+        self._name = name
+        self._pokedexId = pokedexId
+        
+    }
+    
+}
--- Pokedex3/ViewController.swift
+++ Pokedex3/ViewController.swift
@@ -8,16 +8,87 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    @IBOutlet weak var collection: UICollectionView!
+    
+    var pokemon = [Pokemon]()
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collection.dataSource = self
+        collection.delegate = self
+        
+        parsePokemonCSV()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func parsePokemonCSV() {
+        
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
+        
+        do {
+            
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+            print(rows)
+            
+            for row in rows {
+                
+                let pokeID = Int(row["id"]!)!
+                let name = row["identifier"]!
+                
+                let poke = Pokemon(name: name, pokedexId: pokeID)
+                pokemon.append(poke)
+                
+            }
+            
+        } catch let err as NSError {
+            
+            print(err.debugDescription)
+            
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            
+            let pokemon = self.pokemon[indexPath.row]
+            cell.configureCell(pokemon)
+            
+            return cell
+            
+        } else {
+            
+            return UICollectionViewCell()
+            
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return pokemon.count
+        
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 105, height: 105)
+        
     }
 
 
